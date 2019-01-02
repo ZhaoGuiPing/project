05b25d243d999172370656e6b786acbbe3013c6c
ZhaoGuiPing <2237838041@qq.com>
2019-01-02 18:44:28 +0800

    个别修改、

--- 我的长城润滑油/js/index1.js
+++ 我的长城润滑油/js/index1.js
@@ -1,2 +1,34 @@
+$(function(){
+	//碰触下拉菜单
+	$("#navlift").mouseover(function(){
+		$('#navlift_ul').show()
+		
+	})
+	$("#navlift").mouseout(function(){
+		$("#navlift_ul").hide()
+	})
+	
+//	head over---------------------------------------------------------
+	
 
+	
+	
+	//返回顶部JS
+	var $backToTopTxt = "", $backToTopEle = $('<div class="backToTop"></div>').appendTo($("body"))
+		.text($backToTopTxt).attr("title", $backToTopTxt).click(function() {
+			$("html, body").animate({ scrollTop: 0 }, 120);
+	}), $backToTopFun = function() {
+		var st = $(document).scrollTop(), winh = $(window).height();
+		(st > 0)? $backToTopEle.show(): $backToTopEle.hide();
+		//IE6下的定位
+		if (!window.XMLHttpRequest) {
+			$backToTopEle.css("top", st + winh - 166);
+		}
+	};
+	$(window).bind("scroll", $backToTopFun);
+	$(function() { $backToTopFun(); });
+	
+	
+	
+})
 
