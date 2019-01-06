82bb65fae69f06908c72af025e4bd44ec325aafb
ZhaoGuiPing <2237838041@qq.com>
2019-01-06 14:21:41 +0800

    基本完成，个别效果未实现

--- 我的长城润滑油/js/DingDanCenter.js
+++ 我的长城润滑油/js/DingDanCenter.js
@@ -9,9 +9,22 @@ $(function(){
 	})
 	
 //	head over---------------------------------------------------------
-	
+var setTotalCount = 30;
+        $('#box').paging({
+            initPageNo: 3, // 初始页码
+            totalPages: 30, //总页数
+        //  totalCount: '合计' + setTotalCount + '条数据', // 条目总数    
+            slideSpeed: 600, // 缓动速度。单位毫秒
+            jump: true, //是否支持跳转
+            callback: function(page) { // 回调函数
+                console.log(page);
+            }
+        })
 
-	
+
+
+//-----------------------------------------------------------------------------
+
 	
 	//返回顶部JS
 	var $backToTopTxt = "", $backToTopEle = $('<div class="backToTop"></div>').appendTo($("body"))
